d2d0ed777679733167304081e57da570ad65cc07
anyiecon <anyiedcondiza@gmail.com>
2022-04-24 15:21:30 -0500

    municipality

--- src/Components/Ui/Record/Record.jsx
+++ src/Components/Ui/Record/Record.jsx
@@ -120,58 +120,69 @@ export const Record=()=> {
    }
 
 //Clasificador de municipios por departemento 
-  const [Depart, setDepart] = useState()
+const [Depart, setDepart] = useState()
+const [City, setCity] = useState()
 
-  const url = 'https://raw.githubusercontent.com/marcovega/colombia-json/master/colombia.min.json'
-  
-  const fetchApi = async () => {
-    const responde = await fetch(url)
-    const responseDepart = await responde.json()
-    setDepart(responseDepart)
-  }
-  useEffect(() => {
-    fetchApi()
-  })
+// Apis que retornan datos departamentales y municipales
+const url = 'https://raw.githubusercontent.com/marcovega/colombia-json/master/colombia.min.json'
+const ur = 'https://www.datos.gov.co/resource/xdk5-pm3f.json'
 
-  return (
-    <form onSubmit={HandleSubmit} className="record" >
-      <p>RECORD USERS</p>
-      <div className='files'>
-        <div className='filesOne'>  
-          
-          <input type="name" name='name' value={name} onChange={(e) => setName(e.target.value)} placeholder='enter your name' onKeyUp={handleCharacterName} required></input>
-          <p className='alertIcorrect'>{msgName}</p>
-          <input type="alias" name='alias' value={alias} onChange={(e) => setAlias(e.target.value)} placeholder='enter your alias' onKeyUp={handleCharacterAlias} required></input>
-          <p className='alertIcorrect'>{msgAlias}</p>
-          <input type="email" name='email' value={email} onChange={(e) => setEmail(e.target.value)} placeholder='enter your email' onKeyUp={handleCharacterEmail} required></input>
-          <p className='alertIcorrect'>{msgEmail}</p>
-          <input type="password" name='password' value={password} onChange={(e) => setPassword(e.target.value)} placeholder='enter your password' onKeyUp={handleCharacterPassword} required></input>
-          <p className='alertIcorrect'>{msgPassword}</p>
-        </div>
-        <div className='filesTwo'>
-          <select className='selectDepart' type="department" name='department' value={department} onChange={(e) => setDepartment(e.target.value)} placeholder='enter your departament'>
-                { !Depart ? 'Cargando...'  :
-                    Depart.map((Depart,index) => {
-                      return <option key={index} value={Depart.departamento}>{Depart.departamento} </option>
-                    })
-                  }        
-          </select>
-          <select  className='selectMuni' type="municipality" name='municipality' value={municipality} onChange={(e) => setMunicipality(e.target.value)} placeholder='enter your municipality'>
-                { !Depart ? 'Cargando...' :
-                  Depart.map((Depart,index) => {
-                    return <option key={index} value={Depart.ciudades}>{Depart.ciudades} </option>
-                  })}
-          </select>
-          <input type="address" name='address' value={address} onChange={(e) => setAddress(e.target.value)} placeholder='enter your address' required></input>
-          <input type="phone" name='phone' value={phone} onChange={(e) => setPhone(e.target.value)} placeholder='entert your phone' onKeyUp={handleCharacterPhone} required></input>
-          <p className='alertIcorrect'>{msgPhone}</p>
-          <input type="file" name='photo' onChange={(e) => setPhoto(e.target.files[0])} placeholder='enter your profile picture' required></input> 
-        </div>
-      </div>
-      <label><input type="checkbox" name='terminos' id='terminos' checked={terminos} onClick={handleTerminos} ></input>Al hacer click en "REGISTRARSE", Acepta Nuestras Condiciones, la politica de datos y la politica de cookies.</label> 
-      <p className='alertIcorrect'>{msgCheck}</p>
-       <button type="submit" >Registrarse</button>
-    </form>
-  )
+//Respuesta y declaracion de APIS
+const fetchApi = async () => {
+  const responde = await fetch(url)
+  const responseDepart = await responde.json()
+  setDepart(responseDepart)
 }
+useEffect(() => {
+  fetchApi()
+})
+const secondFetch = async () => {
+  const responsecity = await fetch(ur)
+  const responseCity = await responsecity.json()
+  setCity(responseCity)
+}
+useEffect(() => {
+  secondFetch()
+})
 
+
+return (
+  <form onSubmit={HandleSubmit} className="record" >
+    <div className='files'>
+      <div className='filesOne'>  
+        
+        <input type="name" name='name' value={name} onChange={(e) => setName(e.target.value)} placeholder='enter your name' onKeyUp={handleCharacterName} required></input>
+        <p className='alertIcorrect'>{msgName}</p>
+        <input type="alias" name='alias' value={alias} onChange={(e) => setAlias(e.target.value)} placeholder='enter your alias' onKeyUp={handleCharacterAlias} required></input>
+        <p className='alertIcorrect'>{msgAlias}</p>
+        <input type="email" name='email' value={email} onChange={(e) => setEmail(e.target.value)} placeholder='enter your email' onKeyUp={handleCharacterEmail} required></input>
+        <p className='alertIcorrect'>{msgEmail}</p>
+        <input type="password" name='password' value={password} onChange={(e) => setPassword(e.target.value)} placeholder='enter your password' onKeyUp={handleCharacterPassword} required></input>
+        <p className='alertIcorrect'>{msgPassword}</p>
+      </div>
+      <div className='filesTwo'>
+        <select className='selectDepart' type="department" name='department' value={department} onChange={(e) => setDepartment(e.target.value)} placeholder='enter your departament'>
+              { !Depart ? 'Cargando...'  :
+                  Depart.map((Depart,index) => {
+                    return <option key={index} value={Depart.departamento}>{Depart.departamento} </option>
+                  })
+                }         
+        </select>
+        <select  className='selectMuni' type="municipality" name='municipality' value={municipality} onChange={(e) => setMunicipality(e.target.value)} placeholder='enter your municipality'>
+              { !City ? 'Cargando...' :
+                City.map((City,index) => {
+                  return <option key={index} value={City.municipio}> {City.municipio} </option>
+                })}
+        </select>
+        <input type="address" name='address' value={address} onChange={(e) => setAddress(e.target.value)} placeholder='enter your address' required></input>
+        <input type="phone" name='phone' value={phone} onChange={(e) => setPhone(e.target.value)} placeholder='entert your phone' onKeyUp={handleCharacterPhone} required></input>
+        <p className='alertIcorrect'>{msgPhone}</p>
+        <input type="file" name='photo' onChange={(e) => setPhoto(e.target.files[0])} placeholder='enter your profile picture' required></input> 
+      </div>
+    </div>
+    <label><input type="checkbox" name='terminos' id='terminos' checked={terminos} onClick={handleTerminos} ></input>Al hacer click en "REGISTRARSE", Acepta Nuestras Condiciones, la politica de datos y la politica de cookies.</label> 
+    <p className='alertIcorrect'>{msgCheck}</p>
+     <button type="submit" >Registrarse</button>
+  </form>
+)
+}
